6b82b724dc234e25ade4c032baa8fd99a1b2d64a
Verouge <branola1998628@gmail.com>
2023-10-29 02:21:12 +1100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Ensure this is hashed using something like bcrypt before saving.
-  email: { type: String, required: true, unique: true },
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  boards: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Board",
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", userSchema);
