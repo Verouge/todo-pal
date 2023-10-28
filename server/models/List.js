b75f5d5b7d437993b6e17d31cbec037ccf1e8646
Verouge <branola1998628@gmail.com>
2023-10-29 02:22:20 +1100

    Update List.js

--- server/models/List.js
+++ server/models/List.js
@@ -1,6 +1,20 @@
-const listSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  board: { type: mongoose.Schema.Types.ObjectId, ref: "Board", required: true },
+const mongoose = require("mongoose");
+
+const listSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true, // Note: fixed typo from "require" to "required"
+  },
+  cards: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Card",
+    },
+  ],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Board",
+  },
 });
 
 module.exports = mongoose.model("List", listSchema);
