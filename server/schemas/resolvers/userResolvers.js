fc1637db996a25de2bf8994e3d2e6733b596dc3a
Verouge <branola1998628@gmail.com>
2023-10-29 16:22:39 +1100

    Update userResolvers.js

--- server/schemas/resolvers/userResolvers.js
+++ server/schemas/resolvers/userResolvers.js
@@ -1,76 +1,23 @@
-const bcrypt = require("bcryptjs");
-const userModel = require("../../Models/userModel");
-const { createRandomHexColor } = require("../helperMethods");
-const auth = require("../Middlewares/auth");
+const userService = require("../../Services/userService");
 
 const userResolvers = {
   Query: {
     async getUserById(_, { id }) {
-      try {
-        const user = await userModel.findById(id);
-        return user;
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return await userService.getUser(id);
     },
+
     async getUserByEmail(_, { email }) {
-      try {
-        const user = await userModel.findOne({ email });
-        return {
-          name: user.name,
-          surname: user.surname,
-          color: user.color,
-          email: user.email,
-        };
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return await userService.getUserWithMail(email);
     },
   },
 
   Mutation: {
     async registerUser(_, { input }) {
-      const { name, surname, email, password } = input;
-
-      // Password hashing
-      const salt = bcrypt.genSaltSync(10);
-      const hashedPassword = bcrypt.hashSync(password, salt);
-
-      // Create new user
-      const newUser = new userModel({
-        name,
-        surname,
-        email,
-        password: hashedPassword,
-        color: createRandomHexColor(),
-      });
-
-      try {
-        await newUser.save();
-        const token = auth.generateToken(newUser._id.toString(), newUser.email);
-        return { user: newUser, token };
-      } catch (err) {
-        throw new Error("Email already in use!");
-      }
+      return await userService.register(input);
     },
 
     async loginUser(_, { email, password }) {
-      try {
-        const user = await userModel.findOne({ email });
-        if (!user) {
-          throw new Error("Invalid email or password!");
-        }
-
-        const isValidPassword = bcrypt.compareSync(password, user.password);
-        if (!isValidPassword) {
-          throw new Error("Invalid email or password!");
-        }
-
-        const token = auth.generateToken(user._id.toString(), user.email);
-        return { user, token };
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return await userService.login(email, password);
     },
   },
 };
