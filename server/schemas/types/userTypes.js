51cf6ca1fc238a803a95fcbc0debca9c3deaf490
Verouge <branola1998628@gmail.com>
2023-10-29 16:22:09 +1100

    Create userTypes.js

---
+++ server/schemas/types/userTypes.js
@@ -0,0 +1,37 @@
+const { gql } = require("@apollo/server");
+
+const userTypes = gql`
+  type User {
+    id: ID!
+    name: String!
+    surname: String!
+    email: String!
+    avatar: String
+    color: String
+    boards: [Board!]!
+  }
+
+  extend type Query {
+    getUserById(id: ID!): User
+    getUserByEmail(email: String!): User
+  }
+
+  extend type Mutation {
+    registerUser(input: RegisterInput!): AuthPayload!
+    loginUser(email: String!, password: String!): AuthPayload!
+  }
+
+  input RegisterInput {
+    name: String!
+    surname: String!
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    user: User
+    token: String!
+  }
+`;
+
+module.exports = userTypes;
